bfa3f48437a694ebe0c1bc064e8ef755396d084a
johnnie71 <johnnie.agonz71@gmail.com>
2020-10-19 15:41:22 -0400

    done

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+let win = array.find(game => game.result === "W" )
+ if (win === undefined){
+   return undefined
+ }
+ else {
+   return win.year
+ }
+
+}
